4989d3c3961cadfc7999767d6d190368649c7767
Miyamura80 <eitomiyamura@gmail.com>
2024-09-20 18:55:25 +0100

    🐛Bugfix: Fixed global_config

---
+++ global_config/global_config.py
@@ -0,0 +1,32 @@
+import os
+from typing import Any, Dict
+import yaml
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class DictWrapper:
+    def __init__(self, data):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                setattr(self, key, DictWrapper(value))
+            else:
+                setattr(self, key, value)
+
+class Config:
+    OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY")
+
+    def __init__(self):
+        with open("global_config/global_config.yaml", "r") as file:
+            config_data = yaml.safe_load(file)
+        for key, value in config_data.items():
+            if isinstance(value, dict):
+                setattr(self, key, DictWrapper(value))
+            else:
+                setattr(self, key, value)
+
+    def __getattr__(self, name):
+        raise AttributeError(f"'Config' object has no attribute '{name}'")
+
+# Create a singleton instance
+global_config = Config()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from global_config import global_config
 
 def main():
     print("Hello world")
-    print(global_config.OPENAI_API_KEY)
+    print(global_config.example_parent.example_child)
 
 if __name__ == "__main__":
     main()
