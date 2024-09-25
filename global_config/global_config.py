0b120877a33632d9378c64912b7945e2c066b779
Miyamura80 <eitomiyamura@gmail.com>
2024-09-25 16:07:56 +0100

    ✅Update global_config to be recursively attribute retrivee

--- global_config/global_config.py
+++ global_config/global_config.py
@@ -17,6 +17,7 @@ class DictWrapper:
 
 class Config:
     OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY")
+    HELICONE_API_KEY: str = os.environ.get("HELICONE_API_KEY")
 
     def __init__(self):
         with open("global_config/global_config.yaml", "r") as file:
@@ -29,6 +30,17 @@ class Config:
 
     def __getattr__(self, name):
         raise AttributeError(f"'Config' object has no attribute '{name}'")
+
+    def to_dict(self):
+        def unwrap(obj):
+            if isinstance(obj, DictWrapper):
+                return {k: unwrap(v) for k, v in obj.__dict__.items()}
+            elif isinstance(obj, list):
+                return [unwrap(item) for item in obj]
+            else:
+                return obj
+
+        return {k: unwrap(v) for k, v in self.__dict__.items()}
 
 
 # Create a singleton instance
